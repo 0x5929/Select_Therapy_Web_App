23501c8b2890ab71c483d8843d39448310e65fc9
Kevin Ren <work.kevin.ren@gmail.com>
2016-09-25 15:09:14 +0800

    added client/app/app.js, client/app/dropDownMenu/dropDownMenu.js, and client/index.html

---
+++ client/app/dropDownMenu/dropDownMenu.js
@@ -0,0 +1,13 @@
+(function(){
+	"use strict"
+		app.module("myApp").
+	controller('DropdownCtrl', [$scope, DropdownCtrl]);
+	function DropdownCtrl($scope) {
+		$scope.status = {
+			isopen: false
+		}
+	}
+
+
+
+}());
