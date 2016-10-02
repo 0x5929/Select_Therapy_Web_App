24fcda36a7f09175b5732910e771efb0a7a06320
Kevin Ren <work.kevin.ren@gmail.com>
2016-10-01 21:36:34 -0700

    Hooked up the rest of the programs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 //Set Up
+// need to read more about express and its functions. 10/01/2016
 var express = require('express'); //requiring the express module
 var app = express(); //creating our application with express
 var port = 8080; //set the port
@@ -6,4 +7,5 @@ var port = 8080; //set the port
 app.use(express.static("../client")); //setting up the static file location
 //listen (starting our application with node server.js)
 app.listen(port);
+//learn whats env port
 console.log("magic happens on port: " + port);
