e47dd1de0365d190367bec342ec3259d912ddbd2
Kevin Ren <work.kevin.ren@gmail.com>
2016-10-11 10:24:24 -0700

    added the sign up modal and html forms for both sign in and sign up! next we need to hook it up with server/db for functionailities

--- client/app/userDropdown/userDropdown.js
+++ client/app/userDropdown/userDropdown.js
@@ -3,51 +3,90 @@
 	
 	angular.module('myApp.userDropdown', ['services.looksIntegrationByUIB'])
 		.controller('userDropdownControl', ['$scope', dropdownCtrl]);
-		//empty controller, needed for dropdown action
-		function dropdownCtrl($scope) {
-		
-		}
 	
 	angular.module('myApp.userDropdown')
-		.controller('userModalControl', ['$scope', '$uibModal', '$log', userModalCtrl])
-		.controller('ModalInstanceController', ['$scope', '$uibModalInstance', ModalInstanceController]);
-	//controller function for modalinstance
-				function ModalInstanceController($scope, $uibModalInstance) {
-					var ModalInstanceCtrl = this;
-					ModalInstanceCtrl.ok = function () {
+		.controller('signInModalControl', ['$scope', '$uibModal', '$log', signInModalControl])
+		.controller('signUpModalControl', ['$scope', '$uibModal', '$log', signUpModalControl])
+		.controller('signInModalInstanceController', ['$scope', '$uibModalInstance', signInModalInstanceController])
+		.controller('signUpModalInstanceController', ['$scope', '$uibModalInstance', signUpModalInstanceController]);
+				//empty controller, needed for dropdown action
+				function dropdownCtrl($scope) {
+				
+				}
+	//controller function for signInModalInstanceController
+				function signInModalInstanceController($scope, $uibModalInstance) {
+					var signInModalInstanceCtrl = this;
+					signInModalInstanceCtrl.ok = function () {
 					    $uibModalInstance.close('hello');
 					};
-					ModalInstanceCtrl.cancel = function () {
+					signInModalInstanceCtrl.cancel = function () {
 		    			$uibModalInstance.dismiss('cancel');
 		  			};
 				}
-	//function for userModalCtrl
-	function userModalCtrl($scope, $uibModal, $log) {
-		var self = this;
-		this.animationsEnabled = true;
-   	    this.openModal = function (size) {
-		    var modalInstance = $uibModal.open({
-		      animation: this.animationsEnabled,
-		      ariaLabelledBy: 'modal-title',
-		      ariaDescribedBy: 'modal-body',
-		      templateUrl: 'app/userDropdown/view/english/signIn/signInModal.html', 
-		      controller: 	'ModalInstanceController',
-		      controllerAs: 'ModalInstanceCtrl',
-		      size: size,
-		      resolve: {//maybe resolve user data from cookies?
-		        user: function () {
-		          return;
-		        }
-		      }
-		    });
+	//controller function for signUpModalInstanceController
+				function signUpModalInstanceController($scope, $uibModalInstance) {
+					var signUpModalInstanceCtrl = this;
+					signUpModalInstanceCtrl.ok = function () {
+					    $uibModalInstance.close('hello');
+					};
+					signUpModalInstanceCtrl.cancel = function () {
+		    			$uibModalInstance.dismiss('cancel');
+		  			};
+				}
+	//function for signInModalControl
+				function signInModalControl($scope, $uibModal, $log) {
+					var self = this;
+					this.animationsEnabled = true;
+			   	    this.openModal = function (size) {
+					    var modalInstance = $uibModal.open({
+					      animation: this.animationsEnabled,
+					      ariaLabelledBy: 'modal-title',
+					      ariaDescribedBy: 'modal-body',
+					      templateUrl: 'app/userDropdown/view/english/signIn/signInModal.html', 
+					      controller: 	'signInModalInstanceController',
+					      controllerAs: 'signInModalInstanceCtrl',
+					      size: size,
+					      resolve: {//maybe resolve user data from cookies?
+					        user: function () {
+					          return;
+					        }
+					      }
+					    });
 
-		    modalInstance.result.then(function () {	//when modal is closed 
-		      //do something when the modal is closed
-		      console.log('hello world from modalInstance result promise');//logs it onto the client
-		    }, function () {	//when the modal is dismissed by cancel
-		      $log.info('Modal dismissed at: ' + new Date());	//logs the modal dimiss time info on client side
-		   });
-		};
-	}
+					    modalInstance.result.then(function () {	//when modal is closed 
+					      //do something when the modal is closed
+					      console.log('hello world from signInmodalInstance result promise');//logs it onto the client
+					    }, function () {	//when the modal is dismissed by cancel
+					      $log.info('Modal dismissed at: ' + new Date());	//logs the modal dimiss time info on client side
+					   });
+					};
+				}
+				//function for signUpModalControl
+				function signUpModalControl($scope, $uibModal, $log) {
+					var self = this;
+					this.animationsEnabled = true;
+					this. openModal = function(size) {
+						var modalInstance = $uibModal.open({
+					      animation: this.animationsEnabled,
+					      ariaLabelledBy: 'modal-title',
+					      ariaDescribedBy: 'modal-body',
+					      templateUrl: 'app/userDropdown/view/english/signUp/signUpModal.html', 
+					      controller: 	'signUpModalInstanceController',
+					      controllerAs: 'signUpModalInstanceCtrl',
+					      size: size,
+					      resolve: {//maybe resolve user data from cookies?
+					        user: function () {
+					          return;
+					        }
+					      }
+					    });
+					    modalInstance.result.then(function () {	//when modal is closed 
+					      //do something when the modal is closed
+					      console.log('hello world from signUpmodalInstance result promise');//logs it onto the client
+					    }, function () {	//when the modal is dismissed by cancel
+					      $log.info('Modal dismissed at: ' + new Date());	//logs the modal dimiss time info on client side
+					   });
+					}
+				}
 
 }());
