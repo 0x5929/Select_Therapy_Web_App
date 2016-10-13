a65d7c0dd092964daf91eff97b9bf083bbdfeb60
Kevin Ren <work.kevin.ren@gmail.com>
2016-10-13 10:15:11 -0700

    added new route folder, and configurated the database with mongoose and created a model named users for the db, the pw hashing is done as a schema method so it can be included within the model for the db configuration

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
 (function() {
 	'use strict';
-//Set Up
+//Set Up Application
 // need to read more about express and its functions. 10/01/2016
-var fs = require('fs'); 	//requiring fileSystem
-var express = require('express'); 	//requiring the express module
-var bodyParser = require('body-parser');	//fetching body parser
-var path = require('path'); 	//requiring the path module for pdf path resolving
-var nodemailer = require('nodemailer'); 	//fetching nodemailer
-var app = express(); 	//creating our application with express
-var port = process.env.PORT || 8080; 	//set the port
+//fetching all tools
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var mongoose = require('mongoose');	
+var configDB = require('./config/database.js')
+var app = express();
+var port = process.env.PORT || 8080;
+
+//configuration
+mongoose.connect(configDB.url)
 //Setting up express application
 app.use(express.static("../client")); 	//setting up the static file location
 app.use(bodyParser.json());	//get information from html forms
+
 //routes, passing in all the necessary module objs
-require('./routes.js')(app, fs, bodyParser, nodemailer);
+require('./routes/routes.js')(app, fs, bodyParser, nodemailer);
 
 //firing this baby up
 app.listen(port, console.log('magic happens on port: ', port));
