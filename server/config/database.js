854790cebb725ac1fbeef6bbd83bef1427d3000d
Kevin Ren <work.kevin.ren@gmail.com>
2016-10-13 10:15:46 -0700

    added new route folder, and configurated the database with mongoose and created a model named users for the db, the pw hashing is done as a schema method so it can be included within the model for the db configuration

---
+++ server/config/database.js
@@ -0,0 +1,8 @@
+(function() {
+	'use strict';
+//remember to see and check how to configure a password for the database
+	module.exports = {	//exposing the database url, here we have the localhosting db url
+		'url': 'mongodb://localhost/users'
+	};
+
+}());
---
+++ server/routes/routes.js
@@ -0,0 +1,51 @@
+(function() {
+
+	'use strict';
+
+module.exports = function(app, fs, bodyParser, nodemailer) {	//exposing this file to server, all necessary objs passed in
+
+app.get('/About/:id', function(req, res) {	//listens for signal GET repquests for paths start with /about/PdfID/
+	var id = req.params.id; 	//grabbing the ID parameter
+	console.log(id);	//signal test Terminal logs: School_Catalog
+	var filename = __dirname + '/../../client/app/About/' + id + '/' + id + '.pdf';		//creates the file name
+	var PDFReadstream = fs.createReadStream(filename);	//create a read stream for the file
+	res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');	//setting the header, content disposition, and open new tab option with inline, or download option with attachment
+	res.setHeader('Content-type', 'application/pdf');	//setting the content type to header
+	//use the read stream created by fs and pipe through the write stream object in response read stream obj,
+	// which sends(writes) the actually data to client to be rendered. 
+	PDFReadstream.pipe(res);
+});
+
+app.post('/sendMessage', function(req, res) {	//port listening to POST signal from client with http://localhost:8080/sendMessage
+	// the smtp configuration used for the nodemailer's transport to create transporter object 
+	var smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+        user: 'work.kevin.ren@gmail.com',
+        pass: 'jordan45'
+    	}
+	};
+	var transporter = nodemailer.createTransport(smtpConfig);	//initializes the transportor 
+	var requestData = req.body;	//grabing the data, made possible by bodyParser
+	//mail context 
+	var mailData = {
+		from: '"Select Thearpy Institute Message Inquiry" <work.kevin.ren@gmail.com>',
+		to: 'selecttherapyinstitute@gmail.com',
+		subject: 'Inquiry message from ' + requestData.name + ', with the return address of: ' + requestData.email + '.',
+		text: requestData.message
+	};
+	//sending the mail
+	transporter.sendMail(mailData, function(err, info) {
+		if(err)
+			return console.log(err);
+		console.log(info.response);  //delivery success test, logs some werid response property of the info obj, has headers with gmailsmtp
+
+	});
+	res.json(requestData);	//needs to be inproved.. front end doesnt do anything with this response data doe..
+	res.end();	//ends the signal
+	
+});
+};
+}());
