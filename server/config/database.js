33e1910011ac51559425173f0dde2c7a3b48134f
Kevin Ren <work.kevin.ren@gmail.com>
2016-10-18 08:57:13 -0700

    set the mongoose promise lib to global promise lib in node, because mongoose's promise lib is deprecated

--- server/config/database.js
+++ server/config/database.js
@@ -5,13 +5,14 @@
 	module.exports = function(mongoose){
 	var url = 'mongodb://localhost/users/';
 	var databaseConnectionConfig = function() {
+		mongoose.Promise = global.Promise;	//setting the mongoose promise lib to global because mongoose promise lib is depracated
 		var db = mongoose.connection;
 		db.on('error', console.error.bind(console, 'connection error: '));
 		db.once('open', function() {
   		// Create your schemas and models here.
   		//defining schema for our user model: for mongoose
 		var Users = require('../models/users.js');	//loading all models
-		return Users;
+		
 		});
 	};
 	
