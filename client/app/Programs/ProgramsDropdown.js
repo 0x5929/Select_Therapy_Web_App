09439d291a5a13593e8b5e03ace5c9c4d5222580
Kevin Ren <work.kevin.ren@gmail.com>
2016-11-18 18:01:30 -0800

    got home button to work , now need to work on rest

--- client/app/Programs/ProgramsDropdown.js
+++ client/app/Programs/ProgramsDropdown.js
@@ -3,9 +3,10 @@
 	
 	angular.module('myApp.ProgramsDropdown', ['services.looksIntegrationByUIB'])
 
-		.controller('ProgramsDropdownCtrl', ['$scope', DropdownCtrl]);
+		.controller('ProgramsDropdownControl', ['$scope', DropdownCtrl]);
 	
 	function DropdownCtrl($scope) {
 		//empty controller, but needed for the dropdown action
+		//ProgramsDropdownCtrl.option = false;
 	}
 }());
