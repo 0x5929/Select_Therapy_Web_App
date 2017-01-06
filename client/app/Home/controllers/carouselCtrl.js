4d17c68f581c5fbf89b5bc5c369463295fbb7ab3
rennitbaby <work.kevin.ren@gmail.com>
2017-01-05 21:11:00 -0800

    almost fixed carousel, need to work on it more tmr morning

--- client/app/Home/controllers/carouselCtrl.js
+++ client/app/Home/controllers/carouselCtrl.js
@@ -1,31 +1,32 @@
 (function() {
 	'use strict';
-	angular.module('myApp.home', ['ngAnimate', 'ui.bootstrap'])
+	angular.module('myApp.home', ['services.looksIntegrationByUIB'])
 		.controller('carouselController', ['$scope', carouselCtrlHandler]);
 
 		function carouselCtrlHandler($scope) {
 			var carouselCtrl = this;
 			var numOfImg = 1;
+			var currIndex = 0;
 			carouselCtrl.myInterval = 5000;
 			//need to be set up with pictures
-			carouselCtrl.slides = [{image: 'https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2016/03/1458289957powerful-images3.jpg'}];
-			console.log(this);
+			carouselCtrl.slides = [];
 				console.log('why wont i work');
-				/*
+				
 			carouselCtrl.addSlide = function() {
 				console.log('why wont i work inside addSlide');
-    			var newWidth = 600 + slides.length + 1;
-    			slides.push({	//what if the location is using windows command and need a backslash instead '\' ?
-      				image: '/client/app/Home/view/test' + newWidth + '/300',
-				    text: ['Rotation 1','Rotation 2', 'Rotation 3', 'Rotation 4', 'Rotation 5', 'Rotation 6', 'Rotation 7', 'Rotation 8', 'Rotation 9', 'Rotation 10'][slides.length % numOfImg]
+    			var newWidth = 600 + carouselCtrl.slides.length + 1;
+    			carouselCtrl.slides.push({	//what if the location is using windows command and need a backslash instead '\' ?
+      				image: '/client/app/Home/view/test.jpg',
+				    text: ['Rotation 1','Rotation 2', 'Rotation 3', 'Rotation 4', 'Rotation 5', 'Rotation 6', 'Rotation 7', 'Rotation 8', 'Rotation 9', 'Rotation 10'][carouselCtrl.slides.length % 1],
+    				id: currIndex++
     			});
   			};
  			console.log('right before for');
- 			for (var i = 0; i < carouselCtrl.numOfImg; i++) {
+ 			for (var i = 0; i < 1; i++) {
  				console.log(i);
 				carouselCtrl.addSlide();
 			}
 			console.log('right after for');
-			*/
+			
 		}
 }());
