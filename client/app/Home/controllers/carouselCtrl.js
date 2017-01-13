dcd2c11ca281bd6e61e6b0cf41853ff42a653206
Kevin Ren <work.kevin.ren@gmail.com>
2017-01-13 15:49:04 -0800

    cleaned up some code for the carousel, need to fix the width and height

--- client/app/Home/controllers/carouselCtrl.js
+++ client/app/Home/controllers/carouselCtrl.js
@@ -5,24 +5,59 @@
 
 		function carouselCtrlHandler($scope) {
 			var carouselCtrl = this;
-			var numOfImg = 10;
-			var currIndex = 0;
 			carouselCtrl.myInterval = 3000;
 			//need to be set up with pictures
-			var slides = carouselCtrl.slides = [];
-				
-			carouselCtrl.addSlide = function() {
-    			var newWidth = 600 + slides.length + 1;
-    			slides.push({	//what if the location is using windows command and need a backslash instead '\' ?
-      				image: 'client/app/Home/view/images/test.jpg',
-				    text: ['Rotation 1','Rotation 2', 'Rotation 3', 'Rotation 4', 'Rotation 5', 'Rotation 6', 'Rotation 7', 'Rotation 8', 'Rotation 9', 'Rotation 10'][slides.length % numOfImg],
-    				id: currIndex++
-    			});
-  			};
-
- 			for (var i = 0; i < numOfImg; i++) {
-				carouselCtrl.addSlide();
-			}
-			
+			var slides = carouselCtrl.slides = [
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 1',
+					id: 0
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 2',
+					id: 1
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 3',
+					id: 2
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 4',
+					id: 3
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 5',
+					id: 4
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 6',
+					id: 5
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 7',
+					id: 6
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 8',
+					id: 7
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 9',
+					id: 8
+				},
+				{
+					image: 'app/Home/view/images/test.jpg',
+					text: 'Rotation 10',
+					id: 9
+				}
+			];
 		}
 }());
