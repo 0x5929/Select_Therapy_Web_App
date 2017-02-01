3097dc698ac082fca7076cf22dfa5f24d6789702
rennitbaby <work.kevin.ren@gmail.com>
2017-02-01 08:34:59 -0800

    got refresh to work could work on refreshing the radio buttons doe

--- client/app/staff/controllers/adminAddCtrl.js
+++ client/app/staff/controllers/adminAddCtrl.js
@@ -32,7 +32,7 @@
 				ajaxService.post('/admin/add/', admin_add_ctrl.postData)
 				.then(function(successResponse) {
 					console.log(admin_add_ctrl.postData);
-					admin_add_ctrl.refresh(admin_add_ctrl.postData);
+					admin_add_ctrl.refresh();
 					console.log(successResponse);
 				}, 
 			function(failureResposne) {
@@ -40,12 +40,25 @@
 				});
 			};
 
-			admin_add_ctrl.refresh = function(obj) {
-				for (var key in obj) {
-					obj[key] = '';
-					console.log(key);
-					console.log(obj);
-				}
+			admin_add_ctrl.refresh = function() {	//could be done with array and obj higher order functions
+				admin_add_ctrl.name = '';
+				admin_add_ctrl.phoneNumber = '';
+				admin_add_ctrl.ssn = '';
+				admin_add_ctrl.address = '';
+				admin_add_ctrl.email = '';
+				admin_add_ctrl.program = '';
+				admin_add_ctrl.graduate = '';
+				admin_add_ctrl.tuitionPaid = '';
+				admin_add_ctrl.jobPlaced = '';
+				admin_add_ctrl.fullTimePos = '';
+				admin_add_ctrl.partTimePos = '';
+				admin_add_ctrl.payRate = '';
+				admin_add_ctrl.jobDescription = '';
+				admin_add_ctrl.noJobReason = '';
+				admin_add_ctrl.passedExam = '';
+				admin_add_ctrl.passedOn1st = '';
+				admin_add_ctrl.passedOn2nd = '';
+				admin_add_ctrl.passedOn3rd = '';
 			};
 		};
 }());
