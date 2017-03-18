6cee9a74d4a90b77583af476bc2e0a16f893aeb4
rennitbaby <work.kevin.ren@gmail.com>
2017-03-17 17:30:24 -0700

    completely encapsulated admin search and modify

--- client/app/staff/controllers/adminModifyCtrl.js
+++ client/app/staff/controllers/adminModifyCtrl.js
@@ -45,50 +45,17 @@
 	and successfully manipulates program edit
 */
 			
-
-/*
-	NEW LOGIC FOR MODIFYING/ADDING/DELETING PROGRAMS: [{PROGRAM 1 DETAILS}, {PROGRAM 2 DETAILS}]
-		1. have a edit button shown only if admin
-		2. if edit button is clicked => shows individual edit/delete button and add program button and also a cancel edit button
-			2a. once edit button is clicked, contruct a new program obj and copy the old original programs ready for modification. 
-				2aa. if modifying individual program => find the matching program and edit
-				2ab. if adding new program => add the program details to the new program obj (for edit in step 2a)
-				2ac. if deleting program => delete the matching program in the new program obj
-			2b. push the new program obj into putData
-		3. clicking cancel will remove the new program obj and all of its properties, kind of like a refresh
-		4. also need a check for error, make sure there is no matching programs, and no empty programs (noneSelected as name) in the new contructed program arr, if there is, make sure data dont get sent to server!!
-*/
 			admin_modify_ctrl.putChangesFilter = function(data) {
-				var putDataForProgram;
 				var putDataAfterFilter = {};
-				var newDataForProgram = {};
-				var originalProgram = admin_modify_ctrl.parentScope.studentDetail.program;	//WARNING THIS ORIGINAL PROGRAM IS MODIFIED BY THE MODIFY BTN ABOVE, 
-																							//THUS IS THE MOST UPDATED PROGRAM TO BE USED IN THE LOGIC BELOW IN ORDER TO BE 
-																							//PUSHED INTO THE PUTDATA FILTER OBJ
 				var originalName = $scope.admin_search_ctrl.studentDetail.name;
 				for (var inputField in data) {	//first we filter through the fields that is being editted
-					if (inputField === 'program'){//if putData.program:
-						// if (admin_modify_ctrl.modifyProgramName || admin_modify_ctrl.modifyProgramRotation)	//if modifying program
-						// 	putDataAfterFilter[inputField] = originalProgram;	//original program had been editted in the modify program btn function
-						// if (admin_modify_ctrl.newProgramName && admin_modify_ctrl.newProgramRotation){	//if adding new program
-						// 	newDataForProgram.programName = admin_modify_ctrl.newProgramName;	//setting newDataForProgram obj
-						// 	newDataForProgram.programRotation = admin_modify_ctrl.newProgramRotation;
-						// 	if (admin_modify_ctrl.modifyProgramName || admin_modify_ctrl.modifyProgramRotation){	//if programs are being added and editted at the same time
-						// 		putDataAfterFilter[inputField].push(newDataForProgram);	//pushing the new one is the already editted data obj from earlier condition
-						// 	}
-						// 	else{	//tightly coupled, maybe think of another logic for editted programs
-						// 		originalProgram.push(newDataForProgram);	//pushing this object into the original, which should not be editted because it is hasnt gone through the modify program function
-						// 		putDataAfterFilter[inputField] = originalProgram;	//setting original to the filtered through data obj
-						// 	}
-						// }
-						// if (!putDataAfterFilter[inputField])	//this checks if the two above condidtions: edit or add are not met, 
-						// 	putDataAfterFilter[inputField] = originalProgram;	//if they arent met, the puDataAFterFilter[inputField] part of the obj should be undefined, 
-						// 										//so lets equate the student details array, so we update programs every edit, and account for deleted programs
-						putDataAfterFilter[inputField] = admin_modify_ctrl.editProgramArr; //equating the two, makes all code above simplified
+					if (inputField === 'program'){	//if data inputField = program
+						if (admin_modify_ctrl.editProgramArr[0])	//if the editProgramArr has content
+							putDataAfterFilter[inputField] = admin_modify_ctrl.editProgramArr; //equating the two
 					}	//for the rest of the fields, we filter through the ones that is editted
 					else if (data[inputField] !== '' && data[inputField] !== 'noneSelected')	putDataAfterFilter[inputField] = data[inputField];		
 				}
-				//NEEDED TO BE EVLUATED FROM FALSE TO '', BC IF ITS EMPTY STRING, IT WILL BE IGNORE GOING INTO THE SERVER BY THE ABOVE LOGIC
+
 				//this will convert false to empty string to be converted to boolean false or 0 value in the server side.
 				if (putDataAfterFilter.tuitionPaid && putDataAfterFilter.tuitionPaid === 'false')	putDataAfterFilter.tuitionPaid = '';
 				if (putDataAfterFilter.graduate && putDataAfterFilter.graduate === 'false')	putDataAfterFilter.graduate = '';
@@ -100,6 +67,11 @@
 				//returning the filtered through obj
 				return putDataAfterFilter;
 			};
+
+/*
+	err check function, returns true if no err, and false if there is err
+	this way ajax wont be called unless this following function returns true
+*/
 
 			admin_modify_ctrl.noErrCheck = function(dataTobeChecked) {
 						//err conditions
@@ -123,7 +95,7 @@
 					toastFactory.errorToast("please enter a valid email");
 					return false;
 				}
-				if (dataTobeChecked.program[0]){
+				if (dataTobeChecked.program && dataTobeChecked.program[0]){
 					for (var i = 0; i < dataTobeChecked.program.length; i++){
 						if (dataTobeChecked.program[i]['programName'] === 'noneSelected'){
 							toastFactory.errorToast("make sure you choose a valid program name");
@@ -197,25 +169,19 @@
 
 			admin_modify_ctrl.submitChangesBtn = function() {
 				var data = admin_modify_ctrl.putChangesFilter(admin_modify_ctrl.putData);
-				var turnOff = [
-					'showModifyThisProgramBtn',
-					'showAddNewProgramField',
-					'showDeleteProgramBtn'
-				];
 
-				if (admin_modify_ctrl.noErrCheck(data)){
+				if (admin_modify_ctrl.noErrCheck(data)){	//err check
 					ajaxService.put('/admin/modify/', data)
 						.then(function(successResponse) {
 							for (var key in admin_modify_ctrl.putData) {	//clears all data fields when submit is pressed
+								if (key === 'program')
 								admin_modify_ctrl.putData[key] = '';
 							}
 							admin_modify_ctrl.newProgramName = admin_modify_ctrl.newProgramRotation = '';	//clears new program addition field
 							admin_modify_ctrl.modifyProgramName = admin_modify_ctrl.modifyProgramRotation = ''; //clears modify program fields
-							admin_modify_ctrl.turnShowModifyBtnOn();
+							admin_modify_ctrl.turnShowModifyBtnOn();	//turning on the modify button views
 							admin_modify_ctrl.turnEditOff();	//after success submit changes, edit and the rest of ng-ifs are off
-							turnOff.forEach(function(eachTarget) {
-								admin_modify_ctrl[eachTarget] = false;
-							});
+							admin_modify_ctrl.programModifyCancel();
 							toastFactory.sucessEdit();
 							console.log(successResponse);
 						}, function(failureResponse) {	//could add a failure toast
@@ -230,57 +196,43 @@ SHOWING THE MODIFY BUTTON IN VIEW
 AND TURNING OFF THE EDIT FIELD FOR EACH INDIVIDUAL CATEGORY 
 */			
 			admin_modify_ctrl.turnShowModifyBtnOn = function() {
-				var turnOn = [
-					'showModifyNameBtn',
-					'showModifyNumberBtn',
-					'showModifySsnBtn',
-					'showModifyAddressBtn',
-					'showModifyEmailBtn',
-					'showModifyTuitionPaidBtn',
-					'showModifyMarketingSurveyBtn',
-					'showModifyGraduateBtn',
-					'showModifyPassedExamBtn',
-					'showModifyNumberofTriesBtn',
-					'showModifyNoPassReasonBtn',
-					'showModifyJobPlacedBtn',
-					'showModifyWeeklyWorkHoursBtn',
-					'showModifyPayRateBtn',
-					'showModifyJobDescriptionBtn',
-					'showModifyNoJobReasonBtn',
-					'showModifyNotGraduatingReasonBtn'
-
-				];
-
-				//turning on attribute
-				turnOn.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = true;
-				});
+				admin_modify_ctrl.showModifyNameBtn = true;
+				admin_modify_ctrl.showModifyNumberBtn = true;
+				admin_modify_ctrl.showModifySsnBtn = true;
+				admin_modify_ctrl.showModifyAddressBtn = true;
+				admin_modify_ctrl.showModifyEmailBtn = true;
+				admin_modify_ctrl.showModifyTuitionPaidBtn = true;
+				admin_modify_ctrl.showModifyMarketingSurveyBtn = true;
+				admin_modify_ctrl.showModifyGraduateBtn = true;
+				admin_modify_ctrl.showModifyPassedExamBtn = true;
+				admin_modify_ctrl.showModifyNumberofTriesBtn = true;
+				admin_modify_ctrl.showModifyNoPassReasonBtn = true;
+				admin_modify_ctrl.showModifyJobPlacedBtn = true;
+				admin_modify_ctrl.showModifyWeeklyWorkHoursBtn = true;
+				admin_modify_ctrl.showModifyPayRateBtn = true;
+				admin_modify_ctrl.showModifyJobDescriptionBtn = true;
+				admin_modify_ctrl.showModifyNoJobReasonBtn = true;
+				admin_modify_ctrl.showModifyNotGraduatingReasonBtn = true;
 			};
 
 			admin_modify_ctrl.turnEditOff = function() {
-				var turnOff = [	//all the edit fields except programs
-					'editNameFieldOn',
-					'editPhoneFieldOn',
-					'editSsnFieldOn',
-					'editAddressFieldOn',
-					'editEmailFieldOn',
-					'editTuitionPaidFieldOn',
-					'editMarketingSurveyFieldOn',
-					'editGraduateFieldOn',
-					'editPassedExamFieldOn',
-					'editNumberOfTriesFieldOn',
-					'editNoPassReasonFieldOn',
-					'editJobPlacedFieldOn',
-					'editWeeklyWorkingHoursFieldOn',
-					'editPayRateFieldOn',
-					'editJobDescriptionFieldOn',
-					'editNoJobReasonFieldOn',
-					'editNotGraduatingReasonFieldOn'
-				];
-
-				turnOff.forEach(function(eachTarget) {	//turning each edit to false
-					admin_modify_ctrl[eachTarget] = false;
-				});
+				admin_modify_ctrl.editNameFieldOn = false;
+				admin_modify_ctrl.editPhoneFieldOn = false;
+				admin_modify_ctrl.editSsnFieldOn = false;
+				admin_modify_ctrl.editAddressFieldOn = false;
+				admin_modify_ctrl.editEmailFieldOn = false;
+				admin_modify_ctrl.editTuitionPaidFieldOn = false;
+				admin_modify_ctrl.editMarketingSurveyFieldOn = false;
+				admin_modify_ctrl.editGraduateFieldOn = false;
+				admin_modify_ctrl.editPassedExamFieldOn = false;
+				admin_modify_ctrl.editNumberOfTriesFieldOn = false;
+				admin_modify_ctrl.editNoPassReasonFieldOn = false;
+				admin_modify_ctrl.editJobPlacedFieldOn = false;
+				admin_modify_ctrl.editWeeklyWorkingHoursFieldOn = false;
+				admin_modify_ctrl.editPayRateFieldOn = false;
+				admin_modify_ctrl.editJobDescriptionFieldOn = false;
+				admin_modify_ctrl.editNoJobReasonFieldOn = false;
+				admin_modify_ctrl.editNotGraduatingReasonFieldOn = false;
 			};
 
 //RUNNING THE NECESSARY FUNCTIONS FOR VIEW 
@@ -295,7 +247,7 @@ AND TURNING OFF THE EDIT FIELD FOR EACH INDIVIDUAL CATEGORY
 */
 			admin_modify_ctrl.cancelProgramEditCondition = function() {	
 				if (admin_modify_ctrl.showModifyThisProgramBtn || 
-					(admin_modify_ctrl.showModifyCurrentProgramInputNBtn && admin_modify_ctrl.showModifyCurrentProgramInputNBtn[0]) ||
+					(admin_modify_ctrl.showModifyCurrentProgramInput && admin_modify_ctrl.showModifyCurrentProgramInput[0]) ||
 					 admin_modify_ctrl.showAddNewProgramField || 
 					 admin_modify_ctrl.showDeleteProgramBtn)
 					return true;
@@ -356,166 +308,87 @@ AND TURNING OFF THE EDIT FIELD FOR EACH INDIVIDUAL CATEGORY
 */
 
 			admin_modify_ctrl.editCurrentProgramBtn = function() {
-				var turnOn = [
-					'showModifyThisProgramBtn',
-					'showDeleteProgramBtn',
-					'showSubmitProgramModificationBtn',
-					'showAddProgramBtn'	//need to add show delete button, and add button.	//after those buttons are pressed, show a submit changes button, and turn off final submit changes button
-				];
-				var turnOff = [
-					// 'showDeleteProgramBtn',
-					'showSubmitChangesBtn'
-				];
-
 				// //turning off attribute	
-				turnOff.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = false;
-				});
+				admin_modify_ctrl.showSubmitChangesBtn = false;
 				//turning on attribute
-				turnOn.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = true;
-				});
-
+				admin_modify_ctrl.showModifyThisProgramBtn = true;
+				admin_modify_ctrl.showDeleteProgramBtn = true;
+				admin_modify_ctrl.showSubmitProgramModificationBtn = true;
+				admin_modify_ctrl.showAddProgramBtn = true;
+				// copying the original program array into the the edit program array
 				admin_modify_ctrl.parentScope.studentDetail.program.forEach(function(eachProgram) {
 					admin_modify_ctrl.editProgramArr.push(eachProgram);	//copying the original program into edit array.
 				});
 
 			};
 
-			admin_modify_ctrl.addNewProgramBtn = function() {
-				var turnOn = [
-					'showAddNewProgramField'
-				];
-				var turnOff = [
-					'showDeleteProgramBtn',
-					'showSubmitChangesBtn'
-				];
-
-				//turning on attribute
-				turnOn.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = true;
-				});
-				//turning off attributes
-				turnOff.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = false;
-				});
-
-				admin_modify_ctrl.turnEditOff();
-			};
-
-			admin_modify_ctrl.deleteProgramBtn = function() {	//this function is activated when the delete program is first pressed, initializing the delete btns for each program
-				var turnOn = [
-					'showDeleteProgramBtn'
-				];
-				var turnOff = [
-					'showAddNewProgramField',
-					'showModifyThisProgramBtn',
-					'showSubmitChangesBtn'
-				];
-
-				//turning on attribute
-				turnOn.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = true;
-				});
-				//turning off attributes
-				turnOff.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = false;
-				});
-
-				admin_modify_ctrl.turnEditOff();
-
-			};
-
 			admin_modify_ctrl.modifyThisProgramBtn = function(selectedProgram) {
 				var programIndex = admin_modify_ctrl.editProgramArr.indexOf(selectedProgram);
-				var turnOff = [
-					'showModifyThisProgramBtn',
-					'showFinalCancelBtn',
-					'showSubmitChangesBtn'
-				];
-
 				//turning on attribute
 				admin_modify_ctrl.showModifyCurrentProgramInput[programIndex] = true;
 				//turning off attributes
-				turnOff.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = false;
-				});
+				admin_modify_ctrl.showModifyThisProgramBtn = false;
+				admin_modify_ctrl.showFinalCancelBtn = false;
+				admin_modify_ctrl.showSubmitChangesBtn = false;
 			};
 
 			admin_modify_ctrl.programModifyCancel = function() {
-				var turnOn = [
-					'showFinalCancelBtn'
-				];
-				var turnOff = [
-					'showModifyThisProgramBtn',
-					'showAddNewProgramField',
-					'showDeleteProgramBtn',
-					'showAddProgramBtn'
-				];
 
-				//turning on attribute
-				turnOn.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = true;
-				});
+				// //turning on attribute
+				admin_modify_ctrl.showFinalCancelBtn = true;
 				admin_modify_ctrl.turnShowModifyBtnOn();
 				//turning off attributes
-				turnOff.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = false;
-				});
+				admin_modify_ctrl.showModifyThisProgramBtn = false;
+				admin_modify_ctrl.showSubmitProgramModificationBtn = false;
+				admin_modify_ctrl.showAddNewProgramField = false;
+				admin_modify_ctrl.showDeleteProgramBtn = false;
+				admin_modify_ctrl.showAddProgramBtn = false;
 
 				//turning off show modify current program input and button
-				admin_modify_ctrl.showModifyCurrentProgramInput.forEach(function(value) {
-					value = false;
-				});
-
+				for (var i = 0; i <= admin_modify_ctrl.showModifyCurrentProgramInput.length - 1; i++){
+					admin_modify_ctrl.showModifyCurrentProgramInput[i] =  false;
+				}
 				
 			};
 
 			admin_modify_ctrl.finalCancelBtn = function() {
-				var turnOff = [
-					'showModifyThisProgramBtn',
-					'showDeleteProgramBtn',
-					'showAddNewProgramField',
-					'showModifyThisProgramBtn'
-				];
-				turnOff.forEach(function(eachTarget) {
-					admin_modify_ctrl[eachTarget] = false;
-				});
-
+				//turning off attributes
+				admin_modify_ctrl.showModifyThisProgramBtn = false;
+				admin_modify_ctrl.showDeleteProgramBtn = false;
+				admin_modify_ctrl.showAddNewProgramField = false;
+				admin_modify_ctrl.showModifyThisProgramBtn = false;
+				//turn all edit field off
 				admin_modify_ctrl.turnEditOff();
 			};
 
 /*
 	Other action button functionalities. 
 */
-//this functionality needs to be changed, where we modify the new program array with each program obj, and returning that array, and never touching the original program arr
-			admin_modify_ctrl.modifyProgram = function(programObj) {	//to be used by submit modification button; controller function logic below
-				var originalProgram = admin_modify_ctrl.parentScope.studentDetail.program;
-				originalProgram.forEach(function(eachProgram) {
-					if (eachProgram.programName !== programObj.programName)	return;	//skip to next program obj if its not matched
-					else{	//once matched update the program name and rotation field with ng-model in view
-						if (admin_modify_ctrl.modifyProgramName)	eachProgram.programName = admin_modify_ctrl.modifyProgramName;
-						if (admin_modify_ctrl.modifyProgramRotation)	eachProgram.programRotation = admin_modify_ctrl.modifyProgramRotation;
-					}
-					admin_modify_ctrl.submitChangesBtn();	//finally call submit changes again for data to be saved in db, meaning one modification can happen per time.
 
-				});
-			};
 
 			admin_modify_ctrl.submitModificationBtn = function(programToBeModified) {
 				var programIndex = admin_modify_ctrl.editProgramArr.indexOf(programToBeModified);
 				// //modify program
-				// admin_modify_ctrl.modifyProgram(programToBeModified);
 				admin_modify_ctrl.submitChangesBtn();
 				//turn modify current program input and btn off
 				admin_modify_ctrl.showModifyCurrentProgramInput[programIndex] = false;
 			};
 
+			admin_modify_ctrl.addProgramBtn = function() {
+				//creat a new program obj
+				var newProgram = {};
+				//fill program obj with key value pairs
+				newProgram.programName = admin_modify_ctrl.newProgramName;
+				newProgram.programRotation = admin_modify_ctrl.newProgramRotation;
+				//push this new program into the edit arr
+				admin_modify_ctrl.editProgramArr.push(newProgram);
+				//submit the changes
+				admin_modify_ctrl.submitChangesBtn();
+			};
+
 			admin_modify_ctrl.deleteThisProgramBtn = function(programObj) {
-				//delete program
-				var originalProgram = admin_modify_ctrl.parentScope.studentDetail.program;	//accessing parent controller
-				console.log(originalProgram);
-				originalProgram = originalProgram.filter(function(eachProgram) {
+
+				admin_modify_ctrl.editProgramArr = admin_modify_ctrl.editProgramArr.filter(function(eachProgram) {
 					return eachProgram.programName !== programObj.programName;	//returning only obj that dont match with parameter obj, thus deleting that from array
 				});
 				admin_modify_ctrl.submitChangesBtn();	//calling submitChanges so changes can be submmited to database
