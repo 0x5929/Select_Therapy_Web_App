0c43689358f27387dbea4b1dfa510f43af5abbbe
rennitbaby <work.kevin.ren@gmail.com>
2017-04-07 08:08:53 -0700

    started with some configuration, but needs to work on more of the server logic

--- server/config/officegen.js
+++ server/config/officegen.js
@@ -4,7 +4,19 @@
 		//configuration items:
 		//docs
 		//set up for middle ware use maybe?
+		officeGenSetup: officeGenSetup
 	};
+
+	function officeGenSetup() {	//need to work on server logic
+		var configuration = {
+			'type': 'docx',
+			'onend': function(written) {console.log('finished to create, total bytes created: ', written);},
+			'onerr': function(err) {console.log('there is an error: ', err);}
+		};
+
+		var myDoc = officeGenerator(configuration);
+	}
+
 	module.exports = configs;
 
 }());
