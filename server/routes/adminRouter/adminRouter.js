7b8f762c00094ab559018cc57b56cc95b052d835
Kevin Ren <work.kevin.ren@gmail.com>
2017-06-26 17:58:28 -0700

    started to test google append mulitple sheets, fixed the missing speadsheetid

--- server/routes/adminRouter/adminRouter.js
+++ server/routes/adminRouter/adminRouter.js
@@ -272,7 +272,7 @@ REST: ADD
 					CNAPerformanceReport.completionDate= performanceReport.completionDate['CNA']['endDate'];	
 					CNASTRF.title 					   = 'CNASTRF';
 					CNASTRF.course                     = 'Nurse Assistant';
-					CNASTRF.spreadsheetID              = '';
+					CNASTRF.spreadsheetID              = '1b1POFNjX4xlzbtplTZoDwhStOnPajx78Aebmjdvj4wo';
 					//push data to returned
 					returnedData.push(CNAPerformanceReport);
 					returnedData.push(CNASTRF);
@@ -359,44 +359,45 @@ REST: ADD
 				function dbCheckHandler(dataLength, index) {
 					if (index === (dataLength - 1)){
 						//initiate db call
-			// 			var newStudent           	   = new STIDbStudentCollection();
-			// 				newStudent.enrollmentDate  = req.body.enrollmentDate;
-			// 				newStudent.studentID       = req.body.studentID;
-			// 				newStudent.firstName       = req.body.firstName;
-			// 				newStudent.lastName        = req.body.lastName;
-			// 				newStudent.name            = req.body.firstName + ' ' + req.body.lastName;
-			// 				newStudent.phoneNumber     = req.body.phoneNumber;
-			// 				newStudent.ssn             = req.body.ssn;
-			// 				newStudent.address         = req.body.address;
-			// 				newStudent.email           = req.body.email;
-			// 				newStudent.program         = req.body.program;
-			// 				newStudent.tuition         = req.body.tuition;
-			// 				newStudent.graduate        = req.body.graduate;
-			// 				newStudent.tuitionPaid     = req.body.tuitionPaid;
-			// 				newStudent.marketingSurvey = req.body.marketingSurvey;		
-			// //depending on whether or not the student graduated, we save the necessary things to the db and check for pass exam and job place condition, 
-			// //and depending on those conditions we save the necessary data into db	
-			// 			if (req.body.graduate){	//graduate condition
-			// 				newStudent.passedExam = req.body.passedExam;	//saving necesasry properties
-			// 				newStudent.jobPlaced  = req.body.jobPlaced;	//saving necesasry properties
-			// 				if (req.body.passedExam)	newStudent.numberOfTries = req.body.numberOfTries;	//saving necessary properties depending on pass exam condidtion
-			// 				else	newStudent.noPassReason = req.body.noPassReason;	//saving necessary properties depending on pass exam condidtion
-			// 				if (req.body.jobPlaced){
-			// 					newStudent.weeklyWorkHours   = req.body.weeklyWorkHours;	//depending on whether or not the student is employed, we save the necessary things from front end into the db
-			// 					newStudent.payRate           = req.body.payRate;
-			// 					newStudent.placeOfEmployment = req.body.placeOfEmployment;
-			// 					newStudent.employmentAddress = req.body.employmentAddress;
-			// 					newStudent.jobPosition       = req.body.jobPosition;
-			// 				}else	newStudent.noJobReason = req.body.noJobReason;	
-			// 			}
-			// 			else	newStudent.notGraduatingReason = req.body.notGraduatingReason;	//if graduate condition is not met, we then save the none graduate reason
-			// 			//save the new student
-			// 			newStudent.save(newUserSavedHandler);
-						
-			// 			function newUserSavedHandler (err) {
-			// 				if (err) return next(err);
-			// 				else return res.status(200).send('newStudent added').end();	
-			// 			}
+						var newStudent           	   = new STIDbStudentCollection();
+							newStudent.enrollmentDate  = req.body.enrollmentDate;
+							newStudent.studentID       = req.body.studentID;
+							newStudent.firstName       = req.body.firstName;
+							newStudent.lastName        = req.body.lastName;
+							newStudent.name            = req.body.firstName + ' ' + req.body.lastName;
+							newStudent.phoneNumber     = req.body.phoneNumber;
+							newStudent.ssn             = req.body.ssn;
+							newStudent.address         = req.body.address;
+							newStudent.email           = req.body.email;
+							newStudent.program         = req.body.program;
+							newStudent.tuition         = req.body.tuition;
+							newStudent.graduate        = req.body.graduate;
+							newStudent.tuitionPaid     = req.body.tuitionPaid;
+							newStudent.marketingSurvey = req.body.marketingSurvey;	
+							newStudent.googleData      = req.body.google;	
+			//depending on whether or not the student graduated, we save the necessary things to the db and check for pass exam and job place condition, 
+			//and depending on those conditions we save the necessary data into db	
+						if (req.body.graduate){	//graduate condition
+							newStudent.passedExam = req.body.passedExam;	//saving necesasry properties
+							newStudent.jobPlaced  = req.body.jobPlaced;	//saving necesasry properties
+							if (req.body.passedExam)	newStudent.numberOfTries = req.body.numberOfTries;	//saving necessary properties depending on pass exam condidtion
+							else	newStudent.noPassReason = req.body.noPassReason;	//saving necessary properties depending on pass exam condidtion
+							if (req.body.jobPlaced){
+								newStudent.weeklyWorkHours   = req.body.weeklyWorkHours;	//depending on whether or not the student is employed, we save the necessary things from front end into the db
+								newStudent.payRate           = req.body.payRate;
+								newStudent.placeOfEmployment = req.body.placeOfEmployment;
+								newStudent.employmentAddress = req.body.employmentAddress;
+								newStudent.jobPosition       = req.body.jobPosition;
+							}else	newStudent.noJobReason = req.body.noJobReason;	
+						}
+						else	newStudent.notGraduatingReason = req.body.notGraduatingReason;	//if graduate condition is not met, we then save the none graduate reason
+						//save the new student
+						newStudent.save(newUserSavedHandler);
+							console.log('HELLO WORLD NEW STUDENT: ', newStudent);
+						function newUserSavedHandler (err) {
+							if (err) return next(err);
+							else return res.status(200).send('newStudent added').end();	
+						}
 					}
 				}
 
